fdab8642272c72ca628fdb25db3898eae59b81ba
tiny <1533689946@qq.com>
2021-02-23 21:43:28 +0800

    '20210223-2'

---
+++ src/vuex_NoMultiplemodules/state.js
@@ -0,0 +1,15 @@
+/**
+ * 包含N个状态数据属性的对象
+ */
+export default {
+    latitude: 40.10038, // 纬度
+    longitude: 116.36867, // 经度
+    address: {}, // 地址信息对象
+    categorys: [], // 分类数组
+    shops: [], //商家数组
+    user:{}, // 用户信息对象
+    token: localStorage.getItem('token_key') || '', // 当前用户登录的标记
+    goods: [], // 商品列表
+    ratings: [], // 商家评价列表
+    info: {}, // 商家信息
+}
